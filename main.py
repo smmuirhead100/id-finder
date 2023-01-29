4a70765236be34058c8c9984e1b0b6096a12039f
Sean Muirhead <smmuirhead100@berkeley.edu>
2023-01-28 19:13:10 -0800

    allow user input

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-class ID: 
+class id_storage: 
     numbers_names_dict = {}
    
-    def __init__(self, name, ID_number, place): 
+    def __init__(self, name, id_number, place): 
         self.name = name
-        self.ID_number = ID_number
+        self.id_number = id_number
         self.place = place
-        ID.numbers_names_dict[ID_number] = name
+        id_storage.numbers_names_dict[id_number] = name
 
-    def find_ID(self, ID_number, name=None,): 
-        if self.ID_number in ID.numbers_names_dict: 
+    def find_ID(self, id_number, name=None,): 
+        if self.id_number in id_storage.numbers_names_dict: 
             return print("Your ID is currently located at " + str(self.place) + " !")
         else: 
-            return print("Sorry, your ID could not be found!")
+            return print("Sorry, your ID could not be found")
+        
+id_number = input("What is your student ID number?\n")
+name = input("What is your name?\n")
+place = input("Where did you lose your ID?\n")
+first_user = id_storage(id_number, name, place)
+
+first_user.find_ID(id_number, name)
     
